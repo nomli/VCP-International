e1f7f9971624d5afea07d18efe1f4c99900e63b2
nomli <norman_omli@yahoo.com>
2014-06-01 18:15:21 +0800

    june 1 2014

---
+++ js/main.js
@@ -0,0 +1,16 @@
+(function($){
+    ({
+        plugins:function(){
+           
+           
+        },
+        init:function(){
+            var self = this;
+            $.each(self,function(i,ele){
+                if(i !== "init"){
+                    setTimeout(ele,0);
+                }
+            });
+        }
+    }).init();
+})(jQuery);
